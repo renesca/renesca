4f1613d5da6f780a2faab9a25d081adc0c8f18f4
Felix Dietze <felix@beerleader.net>
2014-12-16 17:38:02 +0100

    test with mockito

--- build.sbt
+++ build.sbt
@@ -5,7 +5,7 @@ version := "1.0-SNAPSHOT"
 scalaVersion := "2.11.4"
 
 libraryDependencies ++= Seq(
-  "org.specs2" %% "specs2-core" % "2.4.14" % "test"
+  "org.specs2" %% "specs2" % "2.4.2" % "test"
 )
 
 scalacOptions in Test ++= Seq("-Yrangepos")
--- src/test/scala/GraphSpec.scala
+++ src/test/scala/GraphSpec.scala
@@ -1,16 +1,24 @@
 package renesca
 
 import org.specs2.mutable._
+import org.specs2.mock._
 
-class GraphSpec extends Specification {
+class GraphSpec extends Specification with Mockito {
 
   "Graph" should {
     "run a test" in {
       "Hello world" must have size(11)
     }
+
     "run a test with mock" in {
-      val mockSet = mock[Set]
-      "Hello world" must have size(11)
+      class A {
+        val a = 5
+      }
+      val mockA = mock[A]
+
+      mockA.a returns 6
+
+      mockA.a mustEqual 6
     }
   }
 }

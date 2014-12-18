588fa0496b3e2ae9348394695e76fbbf9f95ca4a
Felix Dietze <felix@beerleader.net>
2014-12-18 21:21:04 +0100

    eliminate sequential in GraphSpec

--- src/test/scala/renesca/GraphSpec.scala
+++ src/test/scala/renesca/GraphSpec.scala
@@ -2,41 +2,32 @@ package renesca
 
 import org.specs2.mock._
 import org.specs2.mutable._
-import org.specs2.specification.BeforeExample
+import org.specs2.specification.Scope
 
-class GraphSpec extends Specification with Mockito with BeforeExample {
-  sequential
+class GraphSpec extends Specification with Mockito {
 
-  var A:Node = null
-  var B:Node = null
-  var ArB:Relation = null
-  var nodes:List[Node] = null
-  var relations:List[Relation] = null
+  trait ExampleGraph extends Scope {
+    val A = mock[Node]
+    val B = mock[Node]
+    val ArB = mock[Relation]
 
-  override def before {
-    A = mock[Node]
-    B = mock[Node]
-    ArB = mock[Relation]
+    val nodes = List(A,B)
+    val relations = List(ArB)
 
-    nodes = List(A,B)
-    relations = List(ArB)
+    val graph = Graph(nodes, relations)
   }
 
   "Graph" should {
-    "create graphs with nodes and relations" in {
-      val graph = Graph(nodes, relations)
-
+    "create graphs with nodes and relations" in new ExampleGraph {
       graph.nodes must containTheSameElementsAs(nodes)
       graph.relations must containTheSameElementsAs(relations)
     }
 
-    "pass a graph instance to each node and relation" in {
-      val graph = Graph(nodes, relations)
-
+    "pass a graph instance to each node and relation" in new ExampleGraph {
       there was one(A).graph_=(graph)
       there was one(B).graph_=(graph)
       there was one(ArB).graph_=(graph)
-
     }
+
   }
 }

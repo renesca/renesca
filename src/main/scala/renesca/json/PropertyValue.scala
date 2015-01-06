8a81ddab93657a3b1ce635c49f7ce560fef1b10f
Felix Dietze <felix@beerleader.net>
2015-01-06 22:00:32 +0100

    implement ParameterValue as super type of PropertyValue

--- src/main/scala/renesca/json/PropertyValue.scala
+++ src/main/scala/renesca/json/PropertyValue.scala
@@ -1,11 +1,17 @@
 package renesca.json
 
-trait PropertyValue
+sealed trait ParameterValue
+sealed trait PropertyValue extends ParameterValue
+
 case class LongPropertyValue(value:Long) extends PropertyValue
 case class DoublePropertyValue(value:Double) extends PropertyValue
 case class StringPropertyValue(value:String) extends PropertyValue
 case class BooleanPropertyValue(value:Boolean) extends PropertyValue
+case object NullPropertyValue extends PropertyValue
 case class ArrayPropertyValue(value:Seq[PropertyValue]) extends PropertyValue
+
+case class ArrayParameterValue(value:Seq[ParameterValue]) extends ParameterValue
+case class MapParameterValue(value:Map[String,ParameterValue]) extends ParameterValue
 
 object PropertyValue {
   implicit def primitiveToPropertyValue(x:Long):PropertyValue = LongPropertyValue(x)
@@ -14,16 +20,22 @@ object PropertyValue {
   implicit def primitiveToPropertyValue(x:String):PropertyValue = StringPropertyValue(x)
   implicit def primitiveToPropertyValue(x:Boolean):PropertyValue = BooleanPropertyValue(x)
 
-  implicit def propertyValueToPrimitive(x:LongPropertyValue):Long = x.value
-  implicit def propertyValueToPrimitive(x:DoublePropertyValue):Double = x.value
-  implicit def propertyValueToPrimitive(x:StringPropertyValue):String = x.value
-  implicit def propertyValueToPrimitive(x:BooleanPropertyValue):Boolean = x.value
-
   implicit def SeqLongToPropertyValue(xs:Seq[Long]):PropertyValue = ArrayPropertyValue(xs map LongPropertyValue)
   implicit def SeqIntToPropertyValue(xs:Seq[Int]):PropertyValue = ArrayPropertyValue(xs.map(x => LongPropertyValue(x.toLong)))
   implicit def SeqDoubleToPropertyValue(xs:Seq[Double]):PropertyValue = ArrayPropertyValue(xs map DoublePropertyValue)
   implicit def SeqStringToPropertyValue(xs:Seq[String]):PropertyValue = ArrayPropertyValue(xs map StringPropertyValue)
   implicit def SeqBooleanToPropertyValue(xs:Seq[Boolean]):PropertyValue = ArrayPropertyValue(xs map BooleanPropertyValue)
+
+  implicit def MapLongToParameterValue(map:Map[String, ParameterValue]):ParameterValue = MapParameterValue(map)
+
+  object collection {
+    def Map(kvs:(String,ParameterValue)*):Map[String,ParameterValue] = scala.collection.immutable.Map(kvs:_*)
+  }
+
+  implicit def propertyValueToPrimitive(x:LongPropertyValue):Long = x.value
+  implicit def propertyValueToPrimitive(x:DoublePropertyValue):Double = x.value
+  implicit def propertyValueToPrimitive(x:StringPropertyValue):String = x.value
+  implicit def propertyValueToPrimitive(x:BooleanPropertyValue):Boolean = x.value
 
   //TODO: ArrayPropertyValue to Array
 }
--- src/main/scala/renesca/json/protocols/ValueProtocol.scala
+++ src/main/scala/renesca/json/protocols/ValueProtocol.scala
@@ -2,46 +2,45 @@ package renesca.json.protocols
 
 import renesca.compat
 import renesca.json._
-import spray.json.{DefaultJsonProtocol, JsArray, JsBoolean, JsNumber, JsObject, JsString, JsValue, RootJsonFormat, deserializationError, serializationError}
+import spray.json._
 
 import scala.collection.mutable
 
 object ValueProtocol extends DefaultJsonProtocol {
 
-  implicit object JsonValueFormat extends RootJsonFormat[PropertyValue] {
+  implicit object JsonPropertyValueFormat extends RootJsonFormat[PropertyValue] {
 
-    override def write(p: PropertyValue) = ???
+    override def write(pv: PropertyValue) = pv match {
+      case LongPropertyValue(value) => JsNumber(value)
+      case DoublePropertyValue(value) => JsNumber(value)
+      case StringPropertyValue(value) => JsString(value)
+      case BooleanPropertyValue(value) => JsBoolean(value)
+      case NullPropertyValue => JsNull
+      case ArrayPropertyValue(value) => JsArray((value map write).toVector)
+    }
 
     override def read(value: JsValue) = value match {
       case JsString(str) => StringPropertyValue(str)
       case JsNumber(num) if num.isValidLong => LongPropertyValue(num.toLong)
       case JsNumber(num) if compat.bigDecimal_isDecimalDouble(num) => DoublePropertyValue(num.toDouble) //TODO: !num.isValidLong ?
       case JsBoolean(bool) => BooleanPropertyValue(bool)
+      case JsNull => NullPropertyValue
       case JsArray(arr) => ArrayPropertyValue(arr.map(read)) // TODO: check type of arr
       case json => deserializationError(s"can not deserialize property value of type $json")
     }
   }
 
-  implicit object PropertiesJsonValueFormat extends RootJsonFormat[Map[String, PropertyValue]] {
-
-    override def write(jsonValues: Map[String, PropertyValue]) = {
-      val jsonMap = jsonValues.map { case (k, v) => k -> toJsValue(v)}.toMap
-      JsObject(jsonMap)
-    }
-
-    def toJsValue(value : PropertyValue) : JsValue =  value match {
-      case StringPropertyValue(str) => JsString(str)
-      case DoublePropertyValue(num) => JsNumber(num)
-      case LongPropertyValue(num) => JsNumber(num)
-      case ArrayPropertyValue(arr) => JsArray((arr map toJsValue).toVector)
-      case other => serializationError(s"can not serialize value of type $other")
+  implicit object JsonParameterValueFormat extends RootJsonFormat[ParameterValue] {
+    override def write(pv: ParameterValue) = pv match {
+      case pv:PropertyValue => JsonParameterValueFormat.write(pv)
+      case ArrayParameterValue(seq) => JsArray((seq map write).toVector)
+      case MapParameterValue(map) => JsObject(map mapValues write)
     }
 
     override def read(value: JsValue) = value match {
-      case JsObject(fields) =>
-        fields.map { case (k, v) => k -> JsonValueFormat.read(v)}.toMap
-      case json => deserializationError(s"can not deserialize property value map of type $json")
+      case x@(_:JsString | _:JsNumber | _:JsBoolean | JsNull) => JsonPropertyValueFormat.read(x)
+      case JsArray(array) => ArrayParameterValue(array map read)
+      case JsObject(map) => MapParameterValue(map mapValues read)
     }
   }
-
 }

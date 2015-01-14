a80be16f7be715da78ef651c28353ea1194840b9
Felix Dietze <felix@beerleader.net>
2015-01-14 15:07:30 +0100

    add integration test infrastructure

---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt.IvyConsole.Dependencies
+import sbt._
+import Keys._
+
+object ApplicationBuild extends Build {
+  import Dependencies._
+
+  lazy val DbTest = config("db") extend(Test)
+  // (other irrelvant ".settings" calls omitted here...)
+
+
+  lazy val project = Project("renesca", file("."))
+
+    // Functional test setup.
+    // See http://www.scala-sbt.org/release/docs/Detailed-Topics/Testing#additional-test-configurations-with-shared-sources
+    .configs(DbTest)
+    .settings(inConfig(DbTest)(Defaults.testSettings) : _*)
+    .settings(
+      testOptions in DbTest := Seq(
+        Tests.Setup(IntegrationTestHelper.setupIntegrationTest _),
+        Tests.Cleanup(IntegrationTestHelper.cleanupIntegrationTest _))
+    )
+}

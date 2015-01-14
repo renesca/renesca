979337f506697103b5c147f305fdf3eb2cacfbd0
Felix Dietze <felix@beerleader.net>
2015-01-14 18:32:53 +0100

    skip integration test when database is not available or empty

---
+++ src/test-integration/scala/renesca/TestSpec2.scala
@@ -0,0 +1,17 @@
+package renesca
+
+class TestSpec2 extends IntegrationSpecification {
+  sequential
+
+  "IntegrationTest" should {
+    "run a Test" in {
+      true mustEqual true
+    }
+
+    "run another Test" in {
+      true mustEqual true
+    }
+  }
+
+}
+

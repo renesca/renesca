e316060965276098c7923ad9773a43b3f41373a2
johannes karoff <johannes@karoff.net>
2015-01-20 18:40:06 +0100

    add test for persistChanges in transaction

--- src/test-integration/scala/renesca/TransactionSpec.scala
+++ src/test-integration/scala/renesca/TransactionSpec.scala
@@ -33,12 +33,28 @@ class TransactionSpec extends IntegrationSpecification {
     "Manual transaction success" in {
       val transaction = new Transaction
       transaction.restService = db.restService // TODO injection
+
       transaction.batchQuery("create (n),(m)")
       val graph = transaction.queryGraph("match (n) return n")
       val List(first, second) = graph.nodes.toList
       transaction.batchQuery("match (n) where id(n) = {id} delete n", Map("id" -> first.id))
-
       transaction.commit()
+
+      val result = db.queryGraph(Query("match n return n"))
+      result.nodes must not contain (first)
+      result.nodes must contain (second)
+    }
+    "Persist graph changes in transaction" in {
+      val transaction = new Transaction
+      transaction.restService = db.restService // TODO injection
+
+      transaction.batchQuery("create (n),(m)")
+      val graph = transaction.queryGraph("match (n) return n")
+      val List(first, second) = graph.nodes.toList
+      graph.delete(first)
+      transaction.persistChanges(graph)
+      transaction.commit()
+
       val result = db.queryGraph(Query("match n return n"))
       result.nodes must not contain (first)
       result.nodes must contain (second)

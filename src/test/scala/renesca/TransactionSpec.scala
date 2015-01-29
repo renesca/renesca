247192fe86d623699acefb3be3be0d672cba7892
Felix Dietze <felix@beerleader.net>
2015-01-29 13:27:57 +0100

    tests for minimizing amount of requests in transactions

--- src/test/scala/renesca/TransactionSpec.scala
+++ src/test/scala/renesca/TransactionSpec.scala
@@ -18,21 +18,67 @@ class TransactionSpec extends Specification with Mockito {
       tx.restService = mock[RestService]
       val statement = "match n return n"
       val jsonRequest = json.Request(List(json.Statement(Query(statement), List("graph"))))
-//      tx.restService.openTransaction() returns
+
+      tx.restService.openTransaction(jsonRequest) returns ((
+        TransactionId("1"),
+        json.Response(
+          commit = Some("http://localhost:7474/db/data/transaction/1/commit"),
+          transaction = Some(json.Transaction("Thu, 29 Jan 2015 11:56:08 +0000"))
+        )))
 
       tx.queryGraph(statement)
       tx.commit()
 
-//      there was one(tx.restService).openTransaction
-//      there was no(tx.restService).resumeTransaction
-//      there was one(tx.restService).commitTransaction
-      true mustEqual true
-    }.pendingUntilFixed
+      there was one(tx.restService).openTransaction(jsonRequest)
+      there was one(tx.restService).commitTransaction(TransactionId("1"))
+
+      there was no(tx.restService).resumeTransaction(TransactionId("1"),jsonRequest)
+    }
   }
 
-  "open transaction on first, resume on second request" in todo
+  "open transaction on first, resume on second request" in {
+    val tx = new Transaction
+    tx.restService = mock[RestService]
+    val statement = "match n return n"
+    val jsonRequest = json.Request(List(json.Statement(Query(statement), List("graph"))))
 
-  "open transaction on commit" in todo
+    tx.restService.openTransaction(jsonRequest) returns ((
+      TransactionId("1"),
+      json.Response(
+        commit = Some("http://localhost:7474/db/data/transaction/1/commit"),
+        transaction = Some(json.Transaction("Thu, 29 Jan 2015 11:56:08 +0000"))
+      )))
+
+    tx.restService.resumeTransaction(TransactionId("1"),jsonRequest) returns json.Response(
+        commit = Some("http://localhost:7474/db/data/transaction/1/commit"),
+        transaction = Some(json.Transaction("Thu, 29 Jan 2015 11:56:08 +0000"))
+      )
+
+    tx.queryGraph(statement)
+    tx.queryGraph(statement)
+    tx.commit()
+
+    there was one(tx.restService).openTransaction(jsonRequest)
+    there was one(tx.restService).resumeTransaction(TransactionId("1"),jsonRequest)
+    there was one(tx.restService).commitTransaction(TransactionId("1"))
+  }
+
+  "open transaction on commit" in {
+    val tx = new Transaction
+    tx.restService = mock[RestService]
+    val statement = "match n return n"
+    val jsonRequest = json.Request(List(json.Statement(Query(statement), List("graph"))))
+
+    tx.restService.singleRequest(jsonRequest) returns json.Response()
+
+    tx.commit(statement)
+
+    there was one(tx.restService).singleRequest(jsonRequest)
+    
+    there was no(tx.restService).openTransaction(jsonRequest)
+    there was no(tx.restService).commitTransaction(TransactionId("1"))
+    there was no(tx.restService).resumeTransaction(TransactionId("1"),jsonRequest)
+  }
 
 }
 

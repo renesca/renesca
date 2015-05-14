e002d42a95aa53068d8238178fda72526717b25b
Felix Dietze <felix@beerleader.net>
2015-05-14 22:48:39 +0200

    simplify local-methods and enhance successors/predecessorsAs

--- src/main/scala/renesca/schema/Factory.scala
+++ src/main/scala/renesca/schema/Factory.scala
@@ -11,10 +11,7 @@ trait NodeFactory[+T <: Node] {
   def wrap(node: raw.Node): T
 }
 
-trait AbstractRelationFactory[+START <: Node, +RELATION <: AbstractRelation[START, END] with Item, +END <: Node] {
-  def startNodeFactory: NodeFactory[START]
-  def endNodeFactory: NodeFactory[END]
-}
+trait AbstractRelationFactory[+START <: Node, +RELATION <: AbstractRelation[START, END] with Item, +END <: Node]
 
 trait RelationFactory[+START <: Node, +RELATION <: Relation[START, END], +END <: Node] extends AbstractRelationFactory[START, RELATION, END] {
   def relationType: raw.RelationType
@@ -22,32 +19,15 @@ trait RelationFactory[+START <: Node, +RELATION <: Relation[START, END], +END <:
 }
 
 trait HyperRelationFactory[
-START <: Node,
++START <: Node,
 STARTRELATION <: Relation[START, HYPERRELATION],
 HYPERRELATION <: HyperRelation[START, STARTRELATION, HYPERRELATION, ENDRELATION, END],
 ENDRELATION <: Relation[HYPERRELATION, END],
-END <: Node] extends NodeFactory[HYPERRELATION] with AbstractRelationFactory[START, HYPERRELATION, END] {
++END <: Node] extends NodeFactory[HYPERRELATION] with AbstractRelationFactory[START, HYPERRELATION, END] {
 
   def startRelationType: raw.RelationType
   def endRelationType: raw.RelationType
 
-  def factory: NodeFactory[HYPERRELATION]
-
-  def startRelationWrap(relation: raw.Relation): STARTRELATION
-  def endRelationWrap(relation: raw.Relation): ENDRELATION
-  def wrap(startRelation: raw.Relation, middleNode: raw.Node, endRelation: raw.Relation): HYPERRELATION = {
-    val hyperRelation = wrap(middleNode)
-    hyperRelation._startRelation = startRelationWrap(startRelation)
-    hyperRelation._endRelation = endRelationWrap(endRelation)
-    hyperRelation
-  }
-
-  def startRelationLocal(startNode: START, middleNode: HYPERRELATION): STARTRELATION = {
-    startRelationWrap(raw.Relation.local(startNode.node, startRelationType, middleNode.node))
-  }
-
-  def endRelationLocal(middleNode: HYPERRELATION, endNode: END): ENDRELATION = {
-    endRelationWrap(raw.Relation.local(middleNode.node, endRelationType, endNode.node))
-  }
+  def wrap(startRelation: raw.Relation, middleNode: raw.Node, endRelation: raw.Relation): HYPERRELATION
 }
 
--- src/main/scala/renesca/schema/Item.scala
+++ src/main/scala/renesca/schema/Item.scala
@@ -10,14 +10,19 @@ import renesca.parameter.implicits._
 //TODO: sealed trait Item and AbstractRelation?
 trait Item
 
-trait Node extends Item with NodeFilter {
+trait Node extends Item with Filter {
   def label = node.labels.head
   def node: raw.Node
   implicit var graph: raw.Graph = null
 
   def neighboursAs[T <: Node](nodeFactory: NodeFactory[T]) = filterNodes(node.neighbours, nodeFactory)
-  def successorsAs[T <: Node](nodeFactory: NodeFactory[T]) = filterNodes(node.successors, nodeFactory)
-  def predecessorsAs[T <: Node](nodeFactory: NodeFactory[T]) = filterNodes(node.predecessors, nodeFactory)
+  def successorsAs[RELNODE <: Node, NODE <: RELNODE](nodeFactory: NodeFactory[NODE], relationFactory: RelationFactory[_, _, RELNODE]) = {
+    filterNodes(node.outRelations.filter(_.relationType == relationFactory.relationType).map(_.endNode), nodeFactory)
+  }
+  //TODO: successorsAs for HyperRelations, same for predecessors
+  def predecessorsAs[RELNODE <: Node, NODE <: RELNODE](nodeFactory: NodeFactory[NODE], relationFactory: RelationFactory[RELNODE, _, _]) = {
+    filterNodes(node.inRelations.filter(_.relationType == relationFactory.relationType).map(_.startNode), nodeFactory)
+  }
   def getStringProperty(key: String) = node.properties(key).asInstanceOf[StringPropertyValue]
 }
 

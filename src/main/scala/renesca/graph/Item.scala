3baf00b213842e9126857e993730c569bf6ead5e
johannes karoff <johannes@karoff.net>
2015-06-16 02:32:00 +0200

    make onMatch properties for Merge Origin optional

--- src/main/scala/renesca/graph/Item.scala
+++ src/main/scala/renesca/graph/Item.scala
@@ -10,5 +10,5 @@ trait Item {
 trait ItemOrigin
 
 case class Create() extends ItemOrigin
-case class Merge(properties: Set[PropertyKey] = Set.empty, onMatch: Set[PropertyKey]) extends ItemOrigin
+case class Merge(properties: Set[PropertyKey] = Set.empty, onMatch: Set[PropertyKey] = Set.empty) extends ItemOrigin
 case class Match() extends ItemOrigin

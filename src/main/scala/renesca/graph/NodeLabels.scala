fe6824b3eed192437a5cf59c33d8eed40e2240e8
Felix Dietze <felix@beerleader.net>
2015-06-22 14:09:36 +0200

    Revert "ability to create unique nodes and relations"
    This reverts commit 1c4834c5b602f695f5c85dbb220ac5eeca91149d.

--- src/main/scala/renesca/graph/NodeLabels.scala
+++ src/main/scala/renesca/graph/NodeLabels.scala
@@ -6,22 +6,18 @@ class NodeLabels(
                   val id: Id,
                   self: mutable.Set[Label] = mutable.HashSet.empty[Label]
                   )
-  extends mutable.Set[Label] with mutable.SetLike[Label, NodeLabels] {
+  extends mutable.Set[Label] with mutable.SetLike[Label, NodeLabels] with ChangeableMember {
 
   private[graph] val localChanges = mutable.ArrayBuffer.empty[GraphChange]
 
   override def +=(label: Label) = {
-    if (!id.isLocal)
-      localChanges += NodeSetLabel(id, label)
-
+    addChange(NodeSetLabel(id, label))
     self += label
     this
   }
 
   override def -=(label: Label) = {
-    if (!id.isLocal)
-      localChanges += NodeRemoveLabel(id, label)
-
+    addChange(NodeRemoveLabel(id, label))
     self -= label
     this
   }
--- src/main/scala/renesca/graph/Properties.scala
+++ src/main/scala/renesca/graph/Properties.scala
@@ -9,25 +9,19 @@ class Properties(val id: Id,
                  setPropertyChange: (Id, PropertyKey, PropertyValue) => GraphChange,
                  removePropertyChange: (Id, PropertyKey) => GraphChange,
                  self: MutablePropertyMap = mutable.HashMap.empty)
-  extends MutablePropertyMap with mutable.MapLike[PropertyKey, PropertyValue, Properties] {
-
-  private var _unique: Option[Seq[PropertyKey]] = None
-  def unique = _unique
-  def unique_=(values: Seq[PropertyKey]) = _unique = Some(values)
+  extends MutablePropertyMap with mutable.MapLike[PropertyKey, PropertyValue, Properties] with ChangeableMember {
 
   private[graph] val localChanges = mutable.ArrayBuffer.empty[GraphChange]
 
   override def +=(keyValue: (PropertyKey, PropertyValue)) = {
     self += keyValue
-    if (!id.isLocal)
-      localChanges += setPropertyChange(id, keyValue._1, keyValue._2)
+    addChange(setPropertyChange(id, keyValue._1, keyValue._2))
     this
   }
 
   override def -=(key: PropertyKey) = {
     self -= key
-    if (!id.isLocal)
-      localChanges += removePropertyChange(id, key)
+    addChange(removePropertyChange(id, key))
     this
   }
 

e9aeca9ee1e09f28943ce1f0c260c72b0f985c46
Felix Dietze <felix@beerleader.net>
2016-10-31 00:04:48 +0100

    fix Pair confusion (scala Pair != org.neo4j.driver.v1.util.Pair)

--- jvm/src/main/scala/renesca/Neo4jTranslation.scala
+++ jvm/src/main/scala/renesca/Neo4jTranslation.scala
@@ -1,6 +1,7 @@
 package renesca
 
 import org.neo4j.driver.v1.Record
+import org.neo4j.driver.v1.util.{Pair => Neo4jPair}
 import org.neo4j.driver.v1.{types => neo4j}
 import renesca.graph._
 import renesca.parameter.PropertyKey
@@ -10,15 +11,17 @@ import scala.collection.mutable
 object Neo4jTranslation {
   def labels(n: neo4j.Node) = n.labels map (s => Label(s))
   def relationType(r: neo4j.Relationship) = RelationType(r.`type`)
-  def properties(e: neo4j.Entity) = e.asMap.map { case (k,v) => PropertyKey(k) -> v}.toMap
+  def properties(e: neo4j.Entity) = e.asMap.map { case (k, v) => PropertyKey(k) -> v }.toMap
 
-  def recordsToGraph(records: Seq[Record]): (Graph, Map[Item,String]) = {
+  def recordsToGraph(records: Seq[Record]): (Graph, Map[Item, String]) = {
     import scala.collection.JavaConversions._
 
     val variableMapping = mutable.Map.empty[Item, String]
 
     val nodes = records.flatMap(_.fields collect {
-      case Pair(varName: String, n: neo4j.Node) =>
+      case p: Neo4jPair[_, _] if p.key.isInstanceOf[String] && p.value.isInstanceOf[neo4j.Node] =>
+        val varName = p.key.asInstanceOf[String]
+        val n = p.value.asInstanceOf[neo4j.Node]
         val node = Node(Id(n.id), labels(n), properties(n))
         variableMapping += node -> varName
         node
@@ -26,7 +29,10 @@ object Neo4jTranslation {
 
     //TODO: more efficient
     val edges = records.flatMap(_.fields collect {
-      case Pair(varName: String, r: neo4j.Relationship) =>
+      case p: Neo4jPair[_, _] if p.key.isInstanceOf[String] && p.value.isInstanceOf[neo4j.Relationship] =>
+        val varName = p.key.asInstanceOf[String]
+        val r = p.value.asInstanceOf[neo4j.Relationship]
+
         val startNode = nodes.find(_.origin.asInstanceOf[Id].id == r.startNodeId).get
         val endNode = nodes.find(_.origin.asInstanceOf[Id].id == r.endNodeId).get
         val relation = Relation(Id(r.id), startNode, endNode, relationType(r), properties(r))
@@ -34,7 +40,7 @@ object Neo4jTranslation {
         relation
     })
 
-     (Graph(nodes, edges), variableMapping.toMap)
+    (Graph(nodes, edges), variableMapping.toMap)
   }
 
 }
